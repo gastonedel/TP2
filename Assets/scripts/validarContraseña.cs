d881ce525146e0e9726d158f55743ad65b6fb9d7
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-04-23 23:44:29 -0300

    Initial commit

---
+++ Assets/scripts/validarContraseña.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class validarContraseña : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousario;
+    public GameObject cartelitoMensaje;
+    public Text mensaje;
+    void Start()
+    {
+        contraseniacorrecta = "12345";
+        cartelitoMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniausuario = ingresousario.text;
+        if (contraseniausuario == contraseniacorrecta)
+        {
+            cartelitoMensaje.SetActive(true);
+            mensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMensaje.SetActive(true);
+            mensaje.text = "Contraseña incorrecta";
+            Debug.Log("contraseña incorrecta");
+        }
+    }
+
+    }
+
+
+
